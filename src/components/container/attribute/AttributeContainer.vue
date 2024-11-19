9495fc99ef97bec836e444cbf321a56fa2c7fdee
qiushiming <qiushiming1997@gmail.com>
2024-11-19 15:05:49 +0800

    feat: 属性面板展开

--- src/components/container/attribute/AttributeContainer.vue
+++ src/components/container/attribute/AttributeContainer.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { useAppStore } from '@/store/app'
+
+const appStore = useAppStore()
+
+function clickContainer() {
+  if (!appStore.attributeVisible) {
+    appStore.toggleAttributeVisible(true)
+  }
+}
+</script>
+
 <template>
-  <div class="attribute-container" :style="{ minWidth: '360px' }">AttributeContainer</div>
+  <div class="attribute-container" :style="{ minWidth: '360px' }" @click="clickContainer">
+    AttributeContainer
+  </div>
 </template>
 
 <style lang="scss" scoped>
